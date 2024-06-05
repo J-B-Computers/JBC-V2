e8cdf48d734a45508c1ff56e1779eebe527a2484
J-B-Computers <jbcom.general@gmail.com>
2024-06-06 02:44:37 +1000

    new

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', (event) => {
     // Highlight the current navigation link
     const navLinks = document.querySelectorAll('.site-navigation-item a');
@@ -12,28 +10,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
     // Smooth scroll to sections
     const smoothScroll = (target, duration) => {
-        const targetPosition = document.querySelector(target).offsetTop;
-        const startPosition = window.pageYOffset;
-        const distance = targetPosition - startPosition;
-        let startTime = null;
-    
-        const animation = (currentTime) => {
-          if (startTime === null) startTime = currentTime;
-          const timeElapsed = currentTime - startTime;
-          const run = ease(timeElapsed, startPosition, distance, duration);
-          window.scrollTo(0, run);
-          if (timeElapsed < duration) requestAnimationFrame(animation);
-        };
-    
-        const ease = (t, b, c, d) => {
-          t /= d / 2;
-          if (t < 1) return c / 2 * t * t + b;
-          t--;
-          return -c / 2 * (t * (t - 2) - 1) + b;
-        };
-    
-        requestAnimationFrame(animation);
+      const targetPosition = document.querySelector(target).offsetTop;
+      const startPosition = window.pageYOffset;
+      const distance = targetPosition - startPosition;
+      let startTime = null;
+  
+      const animation = (currentTime) => {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
       };
+  
+      const ease = (t, b, c, d) => {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+  
+      requestAnimationFrame(animation);
+    };
   
     const scrollToSections = document.querySelectorAll('.scroll-to');
     scrollToSections.forEach(link => {
@@ -59,10 +57,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
       modal.classList.remove('modal-show');
     });
   
-    window.addEventListener('click', (e) => {
-      if (e.target === modal) {
-        modal.classList.remove('modal-show');
-      }
-    });
-  });
+    window.addEventListener('click', (e) =>
   
